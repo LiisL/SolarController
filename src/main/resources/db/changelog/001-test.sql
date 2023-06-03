e394c473e7e72784e2cbd76c75b64398a4fc279b
Nils-Emil <nilsemil.lille@gmail.com>
2023-06-03 15:11:46 +0300

    setup database

---
+++ src/main/resources/db/changelog/001-test.sql
@@ -0,0 +1,8 @@
+--changeset author:id attribute1:value1
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
